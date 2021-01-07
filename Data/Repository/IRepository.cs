3d07ac612bdf78184dcdaa736beb3e0d762cbaea
Ширин Сергей <default-sl@yandex.ru>
2021-01-07 18:22:36 +0700

    Create database interface

---
+++ Data/Repository/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoList.Models;
+
+namespace TodoList.Data.Repository
+{
+    public interface IRepository
+    {
+        IEnumerable<TodoItem> GetAllItems();
+
+        void AddTodo(string todoName);
+        void ValueChanged(TodoItem changedItem);
+        void DeleteItem(int id);
+    }
+}

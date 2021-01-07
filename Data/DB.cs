c436e96b2448d1637630f9c3a499e5aa54330131
Ширин Сергей <default-sl@yandex.ru>
2021-01-07 17:26:57 +0700

    Create DB class

---
+++ Data/DB.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoList.Models;
+
+namespace TodoList.Data
+{
+    public class DB : DbContext
+    {
+        public DB(DbContextOptions<DB> options) : base(options) { }
+        
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
